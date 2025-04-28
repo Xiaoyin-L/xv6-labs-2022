a46549df6319b51f3cf28d9b961e8bb18a3d32e6
yzl <2778910837@qq.com>
2025-04-29 02:42:46 +0800

    finish lab3_usyscall_vmprint

--- kernel/vm.c
+++ kernel/vm.c
@@ -437,3 +437,29 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void
+vmprint(pagetable_t pagetable)
+{
+  int printdeep = 0;
+  if(printdeep == 0){
+    printf("page table %p\n",(uint64)pagetable);
+  }
+  // there are 2^9 = 512 PTEs in a page table.
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if(pte & PTE_V){
+      for(int j = 0; j <= printdeep; j++){
+        printf("..");
+      }
+      printf("%d: pte %p pa %p\n", i, (uint64)pte, (uint64)PTE2PA(pte));
+    }
+    if((pte & PTE_V) && (pte & (PTE_R|PTE_W|PTE_X)) == 0){
+      printdeep++;
+      // this PTE points to a lower-level page table.
+      uint64 child = PTE2PA(pte);
+      vmprint((pagetable_t)child);
+      printdeep--;
+    } 
+  }
+}
