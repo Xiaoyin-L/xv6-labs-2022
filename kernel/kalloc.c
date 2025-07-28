e9ed6007270ce784b87c457d7e751efe1bdb9425
yzl <2778910837@qq.com>
2025-07-28 15:54:03 +0800

    完成 Memory allocator

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -21,12 +21,16 @@ struct run {
 struct {
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+} kmem[NCPU];
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
+  char lockname[8];
+  for(int i=0;i<NCPU;i++){
+    snprintf(lockname, sizeof(lockname), "kmem_%d", i);
+      initlock(&kmem[i].lock, lockname);
+  }
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -56,10 +60,13 @@ kfree(void *pa)
 
   r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  push_off(); //
+  int id = cpuid();
+  acquire(&kmem[id].lock);
+  r->next = kmem[id].freelist;
+  kmem[id].freelist = r;
+  release(&kmem[id].lock);
+  pop_off();
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -70,11 +77,30 @@ kalloc(void)
 {
   struct run *r;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+  push_off();
+  int id = cpuid();
+  acquire(&kmem[id].lock);
+  r = kmem[id].freelist;
+  if(r) //判断当前cpu是否还有空闲页，
+    kmem[id].freelist = r->next;
+  else { // 无空闲则需要窃取
+    int anid; // another id
+    // 遍历所有CPU的空闲列表
+    for(anid = 0; anid < NCPU; anid++){
+      if(anid == id) continue;
+
+      acquire(&kmem[anid].lock);
+      r = kmem[anid].freelist;
+      if(r){
+        kmem[anid].freelist = r->next;
+        release(&kmem[anid].lock);
+        break;
+      }
+      release(&kmem[anid].lock);
+    }
+  }
+  release(&kmem[id].lock);
+  pop_off();
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
